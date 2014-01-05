7dc96420c3de1bbdad82d1df38f88f46e26a596d
agori <agori@localhost.localdomain>
2014-01-05 15:46:44 +0100

    add compojure and ring

--- template/project.clj
+++ template/project.clj
@@ -1,5 +1,6 @@
 (defproject clojureapp "0.1.0-SNAPSHOT"
-    :plugins [[lein-ring "0.7.5"]]
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                   [http-kit "2.1.12"]]
-    :main webapp)
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.6"]
+                 [ring/ring-json "0.2.0"]]
+  :plugins [[lein-ring "0.8.10"]]
+  :ring {:handler clojure-rest.handler/app})
